2aeadee3165ceaa999478b167747bc5c57d69f83
mayud <mayudong200333@gmail.com>
2023-02-08 19:23:12 +0900

    added auth for users

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const path = require('path');
 const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const usersRoutes = require('./routes/user')
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser: true,
@@ -44,15 +47,23 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-
-app.use("/campgrounds",campgrounds)
-app.use("/campgrounds/:id/reviews",reviews)
+app.use('/',usersRoutes);
+app.use("/campgrounds",campgroundsRoutes)
+app.use("/campgrounds/:id/reviews",reviewsRoutes)
 
 app.get('/',(req,res)=>{
     res.render('home')
